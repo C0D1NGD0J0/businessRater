d5dfb71d76cdda835a0ace29bfdb1cc967d2bb98
C0D1NGD0J0 <web-dev@United4Life.local>
2017-06-25 13:22:06 +0100

    user schema defained + passport setup started

---
+++ app/models/user.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const mongoose = require('mongoose'),
+			Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+	fullname:{
+		type: String,
+		required: true
+	},
+	email:{
+		type: String,
+		required: true
+	},
+	password: String,	
+	role: {
+		type: String,
+		default: ''
+	},
+	company:{
+		name: type: String,
+		default: '',
+		image: {type: String, default: ''}
+	},
+	passwordResetToken: {type: String, dafault: ''},
+	passwordResetExpired: {type: Date, default: Date.now}
+});
+
+mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const express = require('express'),
 			mongoose = require('mongoose'),
 			localDB = require('./app/db'),
 			MongoStore = require('connect-mongo')(session),
+			passport = require('passport'),
+			flash = require('connect-flash'),
 			port = (process.env.PORT || 3000),
 			app = express();
 
@@ -17,6 +19,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+require('./app/config/passport');
+
 // SESSION MIDDLEWARE
 app.use(session({
 	secret: config.secret,
@@ -24,6 +28,13 @@ app.use(session({
 	saveUninitialized: false,
 	store: new MongoStore({url: 'mongodb://localhost:27017/rateme-app', autoReconnect: true})
 }));
+
+// FLASH MIDDLEWARE (must do added after session config)
+app.use(flash());
+// PASSPORT MIDDLEWARE
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
