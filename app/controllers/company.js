2512520c6f6d0b517503452f696dfd40e41dbeae
C0D1NGD0J0 <web-dev@United4Life.local>
2017-07-03 17:52:52 +0100

    formidable module added to handle file upload ** feature not implemented properly

--- app/controllers/company.js
+++ app/controllers/company.js
@@ -1,8 +1,31 @@
 'use strict';
+const formidable = require('formidable');
+const path = require('path');
+const fs = require('fs');
 
 let company = {
 	new: (req, res, next) => {
 		res.render('company/company', {title: "Company Registration || RateMe"});
+	},
+
+	upload: (req, res, next) => {
+		let form = new formidable.IncomingForm();
+		
+		form.uploadDir = path.join(__dirname, '../uploads');
+		
+		form.on('file', (field, file) => {
+			fs.rename(file.path, path.join(form.uploadDir, file.name));
+		});
+
+		form.on('error', (err) => {
+			console.log('An error occured', err);
+		});
+		
+		form.on('end', () => {
+			console.log('File upload was successful.')
+		});
+		
+		form.parse(req);
 	}
 }
 
--- app/routes/company.js
+++ app/routes/company.js
@@ -5,5 +5,6 @@ const h = require('../config/helper');
 const company = require('../controllers/company');
 
 router.get('/new', company.new);
+router.post('/upload', company.upload);
 
 module.exports = router;
